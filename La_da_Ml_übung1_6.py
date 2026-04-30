5632c6bb7c01eb7cd61999ce4879db34d1fb6fef
Tristan Tautenhahn <tristantautenhahn@icloud.com>
2026-04-30 12:07:16 +0200

    design_belibige_polynom_matrix gemacht

--- La_da_Ml_übung1_6.py
+++ La_da_Ml_übung1_6.py
@@ -10,7 +10,7 @@ def design_matrix_quadratic(x):
     return np.column_stack((x**2, x, np.ones_like(x)))
 
 def design_beliebige_polynom_matrix(x, p): # p = polynomgrad 
-    return np.column_stack((x**p, x**(p-1), x**(p-2), x**(p-3), np.ones_like(x))) # Achtung Hartcodiert !!!! 
+    return np.column_stack([x**k for k in range(p, -1, -1)]) # -1 wird nicht erreicht --> geht bis 1  
 
 x = np.array([1, 2, 3, 4])
 y = np.array([1.5, 2.5, 3, 4.5])
@@ -23,10 +23,12 @@ A = design_beliebige_polynom_matrix(x, p) # Aufpassen, wenn man reg testen will 
 print(f"Dim(A) = {A.shape}\nA = \n{A}")
 
 #(b)
+
 ATA = A.T @ A # A transponiert mal A 
 ATy = A.T @ y # A transponiert mal y (vektor y) 
+'''
 print(f"Dim(ATA) = {ATA.shape}\nATA = \n{ATA}")
-print(f"Dim(ATy) = {ATy.shape}\nATy = \n{ATy}")
+print(f"Dim(ATy) = {ATy.shape}\nATy = \n{ATy}") '''
 
 # spagetticode unbedingt später beheben!!! 
 def regression_normalgleichung(x, y, p): # x muss nicht übergeben werden, da die funktion design_poly... sich darum kümmert 
@@ -34,6 +36,9 @@ def regression_normalgleichung(x, y, p): # x muss nicht übergeben werden, da di
     lsg = np.array([])
     A = design_beliebige_polynom_matrix(x, p)
     ATA = A.T @ A
+    print(f"Dim(ATA) = {ATA.shape}\nATA = \n{ATA}")
+    ATy = A.T @ y 
+    print(f"Dim(ATy) = {ATy.shape}\nATy = \n{ATy}")
     lsg = np.linalg.inv(ATA) @ A.T @ b
     return lsg
 
